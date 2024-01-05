09bc76a80886d53c489a6dde6dffe59bead1a8f8
VINOTH00712 <154896421+VINOTH00712@users.noreply.github.com>
2024-01-05 18:42:01 +0545

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,11 @@
+let let_obj1 = {name: "person1", age: 5};
+let let_obj2 = {age: 5, name: "person1"};
+if(let_obj1==let_obj2){
+
+console.log("the objects are equal");
+}
+else{
+
+
+console.log("the objects are not equal");
+} 
